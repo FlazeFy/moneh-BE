6cde651ccbe8c3ce375098e64d2b93fc57b9b726
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2024-05-19 07:33:32 +0700

    [TEST] total ammount flow by type, total dct by type, total wishlist type,  total wishlist priority, total wishlist is achieved, total dct to module

--- moneh/tests/e2e/stats_test.go
+++ moneh/tests/e2e/stats_test.go
@@ -47,4 +47,61 @@ var _ = Describe("Moneh API Testing - Stats", func() {
 
 		tests.ValidateResponse(resp, err)
 	})
+
+	It(fmt.Sprintf("%s - Total Ammount Flow By Type", method), func() {
+		client := resty.New()
+		resp, err := client.R().
+			SetHeader("Authorization", fmt.Sprintf("Bearer %s", token)).
+			Get(local_url + "/api/v1/stats/ammountflowtype/" + ord)
+
+		tests.ValidateResponse(resp, err)
+	})
+
+	It(fmt.Sprintf("%s - Total Dct By Type", method), func() {
+		client := resty.New()
+		resp, err := client.R().
+			SetHeader("Authorization", fmt.Sprintf("Bearer %s", token)).
+			Get(local_url + "/api/v1/stats/dcttype/" + ord)
+
+		tests.ValidateResponse(resp, err)
+	})
+
+	It(fmt.Sprintf("%s - Total Wishlist Type", method), func() {
+		client := resty.New()
+		resp, err := client.R().
+			SetHeader("Authorization", fmt.Sprintf("Bearer %s", token)).
+			Get(local_url + "/api/v1/stats/wishlisttype/" + ord)
+
+		tests.ValidateResponse(resp, err)
+	})
+
+	It(fmt.Sprintf("%s - Total Wishlist Priority", method), func() {
+		client := resty.New()
+		resp, err := client.R().
+			SetHeader("Authorization", fmt.Sprintf("Bearer %s", token)).
+			Get(local_url + "/api/v1/stats/wishlistpriority/" + ord)
+
+		tests.ValidateResponse(resp, err)
+	})
+
+	It(fmt.Sprintf("%s - Total Wishlist Is Achieved", method), func() {
+		client := resty.New()
+		resp, err := client.R().
+			SetHeader("Authorization", fmt.Sprintf("Bearer %s", token)).
+			Get(local_url + "/api/v1/stats/wishlistisachieved/" + ord)
+
+		tests.ValidateResponse(resp, err)
+	})
+
+	It(fmt.Sprintf("%s - Total Dictionary To Module", method), func() {
+		table := "flows"
+		col := "flows_category"
+
+		client := resty.New()
+		resp, err := client.R().
+			SetHeader("Authorization", fmt.Sprintf("Bearer %s", token)).
+			Get(local_url + "/api/v1/stats/dctmod/" + table + "/" + col)
+
+		tests.ValidateResponse(resp, err)
+	})
 })
